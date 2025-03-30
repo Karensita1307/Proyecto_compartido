e371de197edc6f3ed1d250935e6f248f2ea1d7dc
Karensita1307 <karen.sofia.lopez@correounivalle.edu.co>
2025-03-29 19:25:51 -0500

    Creación del menú y correción de cositas

--- BatallaPokemones/src/Main.java
+++ BatallaPokemones/src/Main.java
@@ -1,11 +1,63 @@
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Entrenador poque1 = new Entrenador("Carlos", "Equipo 1");
-        poque1.setTipos();
-        poque1.setAtaques();
-        
-    
-    }
+        // Crear un Scanner para leer la entrada del usuario
+        Scanner scanner = new Scanner(System.in);
+        int option;
 
+        do {
+            System.out.println("_____BATALLA DE POKEMONES_____");
+            System.out.println("1. Crear mi equipo pokemon");
+            System.out.println("2. Crear automaticamente mi equipo");
+            System.out.println("3. Salir");
+
+            System.out.print("Seleccione una opción: ");
+            option = scanner.nextInt();
+            scanner.nextLine();//Salto de linea
+
+            switch (option) {
+                case 1:
+                    //Datos del primer entrenador
+                    System.out.print("Ingrese el nombre del  primer entrenador: ");
+                    String nombreEntrenador1 = scanner.nextLine();
+                    System.out.print("Ingrese el nombre del equipo: ");
+                    String nombreEquipo1 = scanner.nextLine();
+                    Entrenador poke1 = new Entrenador(nombreEntrenador1, nombreEquipo1);
+                    poke1.setTipos();
+                    poke1.setAtaques();
+
+                    //Datos finales del primer entrenador 
+                    System.out.println("Entrenador 1: " + poke1.getNombre());
+                    System.out.println("Equipo 1: " + poke1.getEquipo());
+                    System.out.println("Tipos: " + poke1.getTipos());
+                    System.out.println("Ataques: " + poke1.getAtaques());
+
+                    // Crear el segundo entrenador
+                    System.out.print("Ingrese el nombre del  segundo entrenador: ");
+                    String nombreEntrenador2 = scanner.nextLine();
+                    System.out.print("Ingrese el nombre del equipo: ");
+                    String nombreEquipo2 = scanner.nextLine();
+                    Entrenador poke2 = new Entrenador(nombreEntrenador2, nombreEquipo2);
+                    poke2.setTipos();
+                    poke2.setAtaques();
+
+                    //Datos finales del primer entrenador 
+                    System.out.println("Entrenador 1: " + poke2.getNombre());
+                    System.out.println("Equipo 1: " + poke2.getEquipo());
+                    System.out.println("Tipos: " + poke2.getTipos());
+                    System.out.println("Ataques: " + poke2.getAtaques());
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    System.out.println("Saliendo del programa...");
+                    break;
+                default:
+                    System.out.println("Opción no válida. Intente de nuevo.");
+            }
+        } while (option != 3);
+
+        scanner.close();
+    }
 }
--- BatallaPokemones/src/Pokemon.java
+++ BatallaPokemones/src/Pokemon.java
@@ -1,9 +1,15 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
+import utils.Ataques_P;
+import utils.Tipos_P;
 
 public class Pokemon {
     //Atributos
     private String nombre;
     private short puntosSalud;
+    private ArrayList <Ataques_P> ataques = new ArrayList<>();
+    private ArrayList <Tipos_P> tipos = new ArrayList<>();
 
     //Metodos
     public void atacar(){
@@ -16,27 +22,84 @@ public class Pokemon {
     //Constructor 
     public Pokemon(String nombre, short puntosSalud) {
         this.nombre = nombre;
+        ataques = new ArrayList<Ataques_P>();
+        tipos = new ArrayList<Tipos_P>();
         this.puntosSalud = puntosSalud;
-
     }
 
     //Setters
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
+
+    public void setTipos() {
+        Tipos_P listaTipos[] = Tipos_P.values();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Selecciona 3 tipos para el Pokémon:");
+        System.out.println("Tipos disponibles:");
+                for (Tipos_P tipo : listaTipos) {
+                    System.out.println("- " + tipo);
+                }
+        for (int i = 0; i < 3; i++) {
+            while (true) {
+                System.out.print("Selecciona un tipo: ");
+                String tipoS = scanner.nextLine();
+                try {
+                    Tipos_P tipoSeleccionado = Tipos_P.valueOf(tipoS);
+                    tipos.add(tipoSeleccionado); // Agregar el tipo seleccionado a la lista
+                    break; // Salir del bucle si el tipo es válido
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Tipo no válido. Intenta de nuevo.");
+                }
+            }
+        }
+    }
+
+    public void setAtaques() {
+        Ataques_P ListaAtaques[] = Ataques_P.values();
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("selecciona cuatro ataques :");
+        System.out.println("Ataques disponibles:");
+                for(Ataques_P ataque: ListaAtaques){
+                    System.out.println("-" + ataque);
+                }
+        for (int i = 0; i < 4; i++) {
+            while (true) {
+                System.out.print("Selecciona un ataque: ");
+                String ataqueS = scanner.nextLine();
+                try{
+                    Ataques_P ataqueSeleccionado = Ataques_P.valueOf(ataqueS);
+                    ataques.add(ataqueSeleccionado);//Agregar el ataque seleccionado a la lista
+                    break; // Salir del bucle si el tipo es valido
+                } catch (IllegalArgumentException e) {
+                    System.out.println("Ataque no válido. Intenta de nuevo.");
+                }
+            }
+        }
+    }
+
     public void setPuntosSalud(short puntosSalud) {
         this.puntosSalud = puntosSalud;
     }
     
-
     //Getters
     public String getNombre() {
         return nombre;
     }
+
+    public ArrayList<Ataques_P> getAtaques() {
+        return ataques;
+    }
+
+    public ArrayList<Tipos_P> getTipos() {
+        return tipos;
+    }
+
     public short getPuntosSalud() {
         return puntosSalud;
     }
     
-
 
 }
